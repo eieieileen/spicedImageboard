0be439e38f15d0b00f111f7e8c9f4ea65d84193c
Eileen Lassche <eileenlassche@gmail.com>
2021-02-26 18:51:35 +0100

    start pt2

--- public/js/script.js
+++ public/js/script.js
@@ -4,23 +4,50 @@ console.log("sanityyyyy");
 new Vue({
     el: "#main",
     data: {
-        // name: "Morrissey",
-        // seen: true, //also tried true, then it wont show the v-else in index.html
-        images: []
+        images: [],
+        title: "",
+        description: "",
+        username: "",
+        file: null,
     },
     mounted: function () {
-      
         var self = this;
-        axios.get("/images").then(function (response) {
-            self.images = response.data;
-        }).catch(function(err) {
-            console.log("error in axios", err);
-        });
+        axios
+            .get("/images")
+            .then(function (response) {
+                self.images = response.data;
+            })
+            .catch(function (err) {
+                console.log("error in axios", err);
+            });
     },
     methods: {
-        handleClick: function (city) {
-            console.log("handleClick running :D", city);
-            this.seen = !this.seen;
+        handleClick: function (event) {
+            var formData = new FormData();
+            formData.append("title", this.title);
+            formData.append("description", this.description);
+            formData.append("username", this.usernane);
+            formData.append("file", this.file);
+            console.log("this.title:", this.title);
+            console.log("this.description:", this.description);
+            console.log("this.username:", this.username);
+            console.log("this.file: ", this.file);
+
+            axios
+                .post("/upload", formData)
+                .then(function (response) {
+                    console.log("response from post request", response);
+                })
+                .catch(function (err) {
+                    console.log("error from post request", err);
+                });
+        },
+        handleChange: function (event) {
+            console.log("event.target.files[0] ", event.target.file[0]);
+            console.log("handle change is running!");
+            this.file = event.target.files[0];
         }
-    }
+    },
 });
+
+// this.seen = !this.seen;
--- server.js
+++ server.js
@@ -2,6 +2,29 @@ const express = require("express");
 const app = express();
 const db = require("./db");
 
+const multer = require("multer");
+const uidSafe = require("uid-safe");
+const path = require("path");
+
+const diskStorage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, __dirname + "/uploads");
+    },
+    filename: function (req, file, callback) {
+        uidSafe(24).then(function (uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
+    },
+});
+
+const uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152,
+    },
+});
+
+
 app.use(express.static("public"));
 
 
@@ -11,6 +34,21 @@ app.get("/images", (req, res) => {
         res.json(rows);
     }).catch((err) => console.log("error in db.getImages sad puppy 🐶", err));
     console.log("hit the get route!");
+});
+
+app.post("/upload", uploader.single("file"), (req, res) => {
+    console.log("hit the post route...");
+    console.log("req.file: ", req.file);
+    console.log("req.body: ", req.body);
+    if (req.file) {
+        res.json({
+            success: true
+        });
+    } else {
+        res.json({
+            success: false
+        });
+    }
 });
 
 app.listen(8080, () => console.log("My queen, you're going great and you got this 💪"));
